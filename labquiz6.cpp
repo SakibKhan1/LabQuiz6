3767118f5d38664df86236e2387dc18bcfe6c4e0
Sakib Khan <37256061+SakibKhan1@users.noreply.github.com>
2022-04-04 21:33:34 -0400

    Create labquiz6.cpp

---
+++ labquiz6.cpp
@@ -0,0 +1,13 @@
+//Name: Sakib Khan
+//CSCI 135
+//Lab Quiz 5
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 6; i++)
+        cout << "* * * * *" << endl;
+    return 0;
+}
